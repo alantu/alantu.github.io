c2b5afa8a10c1ce1b9478c3c03308fcc966594bc
Ignacio Olaciregui <ignacioola@gmail.com>
2014-10-24 12:57:42 -0300

    optimized images, added video

--- v2/js/main.js
+++ v2/js/main.js
@@ -41,3 +41,29 @@ $('.screen').on('swipe', function() {
   next();
 });
 
+$('[data-target="#video"]').on('click', function (evt) {
+  evt.preventDefault();
+  var $target = $(evt.currentTarget),
+  videoSrc = $target.data('video'),
+  $modal = $("#video");
+
+  $modal.modal('show');
+  $modal
+  .find('iframe').attr('src', videoSrc)
+  .end()
+  .on('hide.bs.modal', function () {
+    $(this)
+    .off('hide.bs.modal')
+    .find('iframe').attr('src', '');
+  })
+  .modal('show');
+});
+
+$('button.arrow').click(function(e) {
+  var form = $(this).parent('form');
+  if ($('input[type=email]', form).val() === '') {
+    e.preventDefault();
+    $('input[type=email]', form).focus();
+    return false;
+  }
+});
